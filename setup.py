340baa5f077b0ae3cb1ab6de736d67be89319c35
Saurabh Kumar <theskumar@users.noreply.github.com>
2015-09-28 21:06:43 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+setup(
+    name="python-usernames",
+    description="Python library to validate usernames suitable for use in public facing applications.",
+    version="0.0.1",
+    author="Saurabh Kumar",
+    author_email="me+github@saurabh-kumar.com",
+    url="http://github.com/theskumar/python-usernames",
+    keywords=['username', 'validation', 'registration', 'python'],
+    py_modules=['usernames'],
+    install_requires=[
+        'click>=5.0',
+    ],
+    entry_points='''
+        [console_scripts]
+        validate_username=usernames:cli
+    ''',
+    classifiers=[
+        # As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.3',
+        # 'Programming Language :: Python :: 2.4',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+        # 'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        # 'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+        # 'Environment :: Web Environment',
+        # 'Framework :: Django',
+    ]
+)
+
+# (*) Please direct queries to the discussion group, rather than to me directly
+#     Doing so helps ensure your question is helpful to other users.
+#     Queries directly to my email are likely to receive a canned response.
+#
+#     Many thanks for your understanding.
