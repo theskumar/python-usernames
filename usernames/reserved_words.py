14f1b834a1af19ab3ede11a37d0e5320ec338504
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-11-28 20:46:27 +0530

    Update keyword list and add reference to https://ldpreload.com/blog/names-to-reserve

--- usernames/reserved_words.py
+++ usernames/reserved_words.py
@@ -7,47 +7,423 @@ a login name or a sub-domain name.
 __References:__
 1. http://www.bannedwordlist.com/
 2. http://blog.postbit.com/reserved-username-list.html
+3. https://ldpreload.com/blog/names-to-reserve
 '''
 
-_d = ("about|access|account|accounts|add|address|adm|admin|administration|adult|"
-      "advertising|affiliate|affiliates|ajax|analytics|android|anon|anonymous|api|"
-      "app|apps|archive|atom|auth|authentication|avatar|"
-      "backup|banner|banners|bin|billing|blog|blogs|board|bot|bots|business|"
-      "chat|cache|cadastro|calendar|campaign|careers|cgi|client|cliente|code|comercial|"
-      "compare|config|connect|contact|contest|create|code|compras|css|"
-      "dashboard|data|db|design|delete|demo|design|designer|dev|devel|dir|directory"
-      "doc|docs|domain|download|downloads|edit|editor|email|ecommerce|"
-      "forum|forums|faq|favorite|feed|feedback|flog|follow|file|files|free|ftp"
-      "gadget|gadgets|games|guest|group|groups|"
-      "help|home|homepage|host|hosting|hostname|html|http|httpd|https|hpg|"
-      "info|information|image|img|images|imap|index|invite|intranet|indice|ipad|iphone|irc|"
-      "java|javascript|job|jobs|js|"
-      "knowledgebase|"
-      "log|login|logs|logout|list|lists|"
-      "mail|mail1|mail2|mail3|mail4|mail5|mailer|mailing|mx|manager|marketing|master|me|media|message|"
-      "microblog|microblogs|mine|mp3|msg|msn|mysql|messenger|mob|mobile|movie|movies|music|musicas|my|"
-      "name|named|net|network|new|news|newsletter|nick|nickname|notes|noticias|ns|ns1|ns2|ns3|ns4|"
-      "old|online|operator|order|orders|"
-      "page|pager|pages|panel|password|perl|pic|pics|photo|photos|photoalbum|php|plugin|plugins|pop|pop3|post|"
-      "postmaster|postfix|posts|profile|project|projects|promo|pub|public|python|"
-      "random|register|registration|root|ruby|rss|"
-      "sale|sales|sample|samples|script|scripts|secure|send|service|shop|sql|signup|signin|search|security|"
-      "settings|setting|setup|site|sites|sitemap|smtp|soporte|ssh|stage|staging|start|subscribe|subdomain|"
-      "suporte|support|stat|static|stats|status|store|stores|system|"
-      "tablet|tablets|tech|telnet|test|test1|test2|test3|teste|tests|theme|themes|tmp|todo|task|tasks|tools|tv|talk|"
-      "update|upload|url|user|username|usuario|usage|"
-      "vendas|video|videos|visitor|"
-      "win|ww|www|www1|www2|www3|www4|www5|www6|www7|wwww|wws|wwws|web|webmail|website|websites|webmaster|workshop|"
-      "xxx|xpg|you|yourname|yourusername|yoursite|yourdomain|")
-
-_d += ("anal|anus|arse|ass|ballsack|balls|bastard|bitch|biatch|bloody|blowjob|bollock|bollok|boner|"
-       "boob|bugger|bum|butt|buttplug|clitoris|cock|coon|crap|cunt|damn|dick|dildo|dyke|fag|feck|"
-       "fellate|fellatio|felching|fuck|fucker|fudgepacker|fudge|packer|flange|Goddamn|God|damn|hell|homo|"
-       "jerk|jizz|knobend|knob|end|labia|lmao|lmfao|motherfucker|muff|nigger|nigga|omg|penis|piss|poop|prick|pube|"
-       "pussy|queer|scrotum|sex|shit|sh1t|slut|smegma|spunk|tit|tosser|turd|twat|vagina|wank|whore|wtf|")
+_d = """
+about
+abuse
+access
+account
+accounts
+add
+address
+adm
+admin
+administration
+administrator
+adult
+advertising
+affiliate
+affiliates
+ajax
+anal
+analytics
+android
+anon
+anonymous
+anus
+api
+app
+apps
+archive
+arse
+ass
+atom
+auth
+authentication
+autoconfig
+avatar
+backup
+balls
+ballsack
+banner
+banners
+bastard
+biatch
+billing
+bin
+bitch
+blog
+blogs
+bloody
+blowjob
+board
+bollock
+bollok
+boner
+boob
+bot
+bots
+broadcasthost
+bugger
+bum
+business
+butt
+buttplug
+cache
+cadastro
+calendar
+campaign
+careers
+cgi
+chat
+client
+cliente
+clitoris
+cock
+code
+comercial
+compare
+compras
+config
+connect
+contact
+contest
+coon
+crap
+create
+css
+cunt
+damn
+dashboard
+data
+db
+delete
+demo
+design
+designer
+dev
+devel
+dick
+dildo
+dir
+directory
+doc
+docs
+domain
+download
+downloads
+dyke
+ecommerce
+edit
+editor
+email
+end
+fag
+faq
+favorite
+feck
+feed
+feedback
+felching
+fellate
+fellatio
+file
+files
+flange
+flog
+follow
+forum
+forums
+free
+ftp
+fuck
+fucker
+fudge
+fudgepacker
+gadget
+gadgets
+games
+god
+goddamn
+group
+groups
+guest
+hell
+help
+home
+homepage
+homo
+host
+hosting
+hostmaster
+hostname
+hpg
+html
+http
+httpd
+https
+image
+images
+imap
+img
+index
+indice
+info
+information
+intranet
+invite
+ipad
+iphone
+irc
+is
+isatap
+it
+java
+javascript
+jerk
+jizz
+job
+jobs
+js
+knob
+knobend
+knowledgebase
+labia
+list
+lists
+lmao
+lmfao
+localdomain
+localhost
+log
+login
+logout
+logs
+mail
+mail1
+mail2
+mail3
+mail4
+mail5
+mailer
+mailer-daemon
+mailing
+manager
+marketing
+master
+me
+media
+message
+messenger
+microblog
+microblogs
+mine
+mis
+mob
+mobile
+motherfucker
+movie
+movies
+mp3
+msg
+msn
+muff
+music
+musicas
+mx
+my
+mysql
+name
+named
+net
+network
+new
+news
+newsletter
+nick
+nickname
+nigga
+nigger
+no-reply
+nobody
+noc
+noreply
+notes
+noticias
+ns
+ns1
+ns2
+ns3
+ns4
+old
+omg
+online
+operator
+order
+orders
+packer
+page
+pager
+pages
+panel
+password
+penis
+perl
+photo
+photoalbum
+photos
+php
+pic
+pics
+piss
+plugin
+plugins
+poop
+pop
+pop3
+post
+postfix
+postmaster
+posts
+prick
+profile
+project
+projects
+promo
+pub
+pube
+public
+pussy
+python
+queer
+random
+register
+registration
+root
+rss
+ruby
+sale
+sales
+sample
+samples
+script
+scripts
+scrotum
+search
+secure
+security
+send
+service
+setting
+settings
+setup
+sex
+sh1t
+shit
+shop
+signin
+signup
+site
+sitemap
+sites
+slut
+smegma
+smtp
+soporte
+spunk
+sql
+ssh
+ssladmin
+ssladministrator
+sslwebmaster
+stage
+staging
+start
+stat
+static
+stats
+status
+store
+stores
+subdomain
+subscribe
+suporte
+support
+sysadmin
+system
+tablet
+tablets
+talk
+task
+tasks
+tech
+telnet
+test
+test1
+test2
+test3
+teste
+tests
+theme
+themes
+tit
+tmp
+todo
+tools
+tosser
+turd
+tv
+twat
+update
+upload
+url
+usage
+usenet
+user
+username
+usuario
+uucp
+vagina
+vendas
+video
+videos
+visitor
+wank
+web
+webmail
+webmaster
+website
+websites
+whore
+win
+workshop
+wpad
+wtf
+ww
+wws
+www
+www1
+www2
+www3
+www4
+www5
+www6
+www7
+wwws
+wwww
+xpg
+xxx
+you
+yourdomain
+yourname
+yoursite
+yourusername
+"""
 
 
 def get_reserved_wordlist():
-    return set(_d.split("|"))
+    return set(_d.splitlines())
 
 __all__ = ['get_reserved_wordlist']
